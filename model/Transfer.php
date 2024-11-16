77799948e771bde9d53934422fd5945df1f22792
FireLifeDanik28 <lifedanik2006@gmail.com>
2024-11-16 19:41:19 +0100

    Uodpornienie API

--- model/Transfer.php
+++ model/Transfer.php
@@ -1,7 +1,34 @@
 <?php
 class Transfer {
     public static function new(int $source, int $target, int $amount, mysqli $db) : void {
+        $db->begin_transaction();
+        if($amount <= 0){
+            //jeżeli wartość jest nie pozytywna to wycofaj transakcje
+            $db->rollback();
+            //rzuć wyjątek
+            throw new Exception('Liczba wynosi mniej niż 0');
+        } else
         try {
+            // sprawdź wartość wysłąną
+            $sql = "SELECT amount FROM account WHERE accountNo = ?";
+            $query = $db->prepare($sql);
+            // podmień numer konta źródłowego na zapytanie
+            $query->bind_param('i', $source);
+            // uzyskaj aktualny stan konta
+            $query->execute();
+            // pobierz wynik zapytania
+            $result = $query->get_result();
+            // Pobierz tablicę
+            $row = $result->fetch_assoc();
+
+            // sprawdź, czy sender nie jest biedakiem
+            if ($row['amount'] < $amount) {
+                // wycofaj transakcje
+                $db->rollback();
+                // rzuć wyjątek
+                throw new Exception('Nie masz kasy biedaku');
+            }
+
             //sql - odjęcie kwoty z rachunku 1
             $sql = "UPDATE account SET amount = amount - ? WHERE accountNo = ?";
             //przygotuj zapytanie
